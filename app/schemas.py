abae10cb5c4d0bcb1371bf5af65cca893393e07d
Jyotirmay Verma <vermajyotirmay54@gmail.com>
2026-03-19 01:31:22 +0530

    pydantic schemas of new tables

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,10 @@
 from pydantic import BaseModel, EmailStr, Field, ConfigDict
 from .constants.user_role import RoleEnum
 from .constants.permissions import PermissionEnum
+from .constants.tasks import TaskTagsEnum, TaskStatus
+from .constants.payment import Currency, PaymentMode
+from .constants.skills import SkillsEnum
+from datetime import datetime
 
 class TokenData(BaseModel):
     token : str
@@ -24,3 +28,70 @@ class CreateMember(UserBase):
 
 class TokenInfo(BaseModel):
     id : int
+
+class Address(BaseModel):
+    id : int
+    uid : int
+    house_number : str
+    landmark : str
+    area : str
+    city : str
+    state : str
+    country : str
+    pincode : str
+
+    model_config= ConfigDict(from_attributes=True)
+
+class CustomerBase(BaseModel):
+    customer_info : UserBase
+    address: Address
+    model_config= ConfigDict(from_attributes=True)
+
+class CustomerResponse(CustomerBase):
+    id : int
+    u_id : int
+
+class TaskerSkill(BaseModel):
+    id : int
+    tasker_id : int
+    tasker_skill : SkillsEnum
+
+    model_config= ConfigDict(from_attributes=True)
+
+class TaskerBase(BaseModel):
+    skills : list[TaskerSkill]
+    tasks : list[TaskBase]
+    model_config= ConfigDict(from_attributes=True)
+
+class TaskerResponse(TaskerBase):
+    id : int
+    u_id : int
+    rating : float | None = 0.0
+    task_completed : int
+    is_delete : bool | None = False
+    is_active : bool | None = False
+    created_at : datetime
+
+class TaskBase(BaseModel):
+    task_title : str
+    task_description : str
+    task_tag : TaskTagsEnum
+    currency: Currency
+    amount : int
+    payment_method : PaymentMode
+    
+    model_config= ConfigDict(from_attributes=True)
+
+class CreateTask(TaskBase):
+    tasker_id : int
+    pass
+
+class TaskResponse(CreateTask):
+    tasker : TaskerResponse
+    task_id : int
+    customer_id : int
+    task_start_date : datetime | None = None
+    task_end_date : datetime | None = None
+    created_at : datetime
+    task_status : TaskStatus | None = TaskStatus.POSTED
+    # customer : 
